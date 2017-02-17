87a2f521ff1c7b7b7c3c835976b5b4de8b52651b
Priscille De Dumast <pdedumast@gmail.com>
2017-02-17 13:46:46 -0500

    STYLE: Clean code to manage the list of mesh to compute the distances

--- src/CondylesFeaturesExtractor/condylesfeaturesextractor.cxx
+++ src/CondylesFeaturesExtractor/condylesfeaturesextractor.cxx
@@ -2,78 +2,40 @@
 #include "condylesfileIO.hxx"
 #include "condylesfeaturesextractorCLP.h"
 
-#include "argio.hh"
-
 #include <dirent.h>
 #include <iterator>
 
-// Declaration of getListFile()
-void getListFile(std::string path, std::vector<std::string> &list, const std::string &suffix);
 
 int main (int argc, char *argv[])
 {
 	PARSE_ARGS;
-
-    const char** argv_const = const_cast<const char**>(argv);
     
-    vtkSmartPointer<CondylesFeaturesExtractor> Filter = vtkSmartPointer<CondylesFeaturesExtractor>::New();
-
     vtkSmartPointer<vtkPolyData> inputShape = readVTKFile(inputMesh.c_str());
     int num_points = inputShape->GetNumberOfPoints();
 
-    // Get list of group mean shapes
-    std::vector<std::string> listMeanFiles;
-    // if (!meanGroupDir.empty())
-        // getListFile(meanGroupDir, listMeanFiles, "vtk");
-    listMeanFiles = distMesh;
-
-    // Load each mean groupe shape & create labels
-    std::vector< vtkSmartPointer<vtkPolyData> > meanShapesList;
-    std::vector<std::string> meanDistLabels;
-    for (int k=0; k<listMeanFiles.size(); k++) 
+    std::vector< vtkSmartPointer<vtkPolyData> > distMeshList;
+    if ( distMeshOn )
     {
-        vtkSmartPointer<vtkPolyData> meanShape = vtkSmartPointer<vtkPolyData>::New();
-        meanShape = readVTKFile( listMeanFiles[k].c_str() );
-
-        if (meanShape->GetNumberOfPoints() != num_points)
+        // Load each mesh used for distances 
+        for (int k=0; k<distMesh.size(); k++) 
         {
-            std::cerr << "All the shapes must have the same number of points" << std::endl;
-            return EXIT_FAILURE;
-        } 
-        meanShapesList.push_back(meanShape);
+            vtkSmartPointer<vtkPolyData> crt_mesh = vtkSmartPointer<vtkPolyData>::New();
+            crt_mesh = readVTKFile( distMesh[k].c_str() );
+
+            if (crt_mesh->GetNumberOfPoints() != num_points)
+            {
+                std::cerr << "All the shapes must have the same number of points" << std::endl;
+                return EXIT_FAILURE;
+            } 
+            distMeshList.push_back(crt_mesh);
+        }
     }
 
-
     // **********
-	Filter->SetInput(inputShape, meanShapesList);
+    vtkSmartPointer<CondylesFeaturesExtractor> Filter = vtkSmartPointer<CondylesFeaturesExtractor>::New();
+	Filter->SetInput(inputShape, distMeshList);
 	Filter->Update();
     writeVTKFile(outputMesh.c_str(),Filter->GetOutput());
 
 	return EXIT_SUCCESS;
 }
-
-
-/** Function  getListFile(std::string path, std::vector<std::string> &list, const std::string &suffix)
- * Recupere la liste des fichier dans un dossier
- * @param path : directory of group mean shapes
- * @param list : list of group mean shape
- * @param suffix : files extension
- */
-
-void getListFile(std::string path, std::vector<std::string> &list, const std::string &suffix)
-{
-    DIR *dir = opendir(path.c_str());
-    if (dir != NULL)
-    {
-        while (dirent *entry = readdir(dir))
-        {
-            std::string filename = entry->d_name;
-            if (filename.size() >= suffix.size() && equal(suffix.rbegin(), suffix.rend(), filename.rbegin()))
-            list.push_back(path + "/" + filename);
-        }
-    }
-    closedir(dir);
-    sort(list.begin(), list.begin() + list.size());
-}
-
-
